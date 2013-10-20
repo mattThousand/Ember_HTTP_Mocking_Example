3308eba0806667ecec0553d145c61b8ab6221e83
mattThousand <matt.d.buckley1212@gmail.com>
2013-10-20 18:49:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberHttpMockingExample::Application.config.secret_token = '2fa5ceea8329ffee621cfcf65d9bc0741fff1ec82cc71ad24626d82bd0461e53c132fc4e83684727fa93674f48ee0fef2221f99ac45e24e3440ab03373fdb566'
